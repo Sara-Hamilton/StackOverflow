f957e8228cd99732a72dc7d60a51dbbfc7fd24c1
Sara Hamilton and Stephanie Faber <sara@indeterminate.net and eyesicedover@gmail.com>
2018-04-30 10:32:08 -0700

    modify startup.cs, add ApplicationDbContext, and add ApplicationUser to use Authentication

--- StackOverflow/Startup.cs
+++ StackOverflow/Startup.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using StackOverflow.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace StackOverflow
@@ -29,10 +33,27 @@ namespace StackOverflow
         {
             // Add framework services.
             services.AddMvc();
+            services.AddEntityFrameworkMySql()
+                   .AddDbContext<ApplicationDbContext>(options =>
+                                             options
+                                                  .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
+
+            // This is new
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 0;
+                options.Password.RequireDigit = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, LoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -49,11 +70,17 @@ namespace StackOverflow
 
             app.UseStaticFiles();
 
+            app.UseIdentity();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Account}/{action=Index}/{id?}");
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
