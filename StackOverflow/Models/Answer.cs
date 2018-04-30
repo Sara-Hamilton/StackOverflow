092b05ac06fb5e0faf62d671ecd3cb05afc79637
Sara Hamilton and Stephanie Faber <sara@indeterminate.net and eyesicedover@gmail.com>
2018-04-30 10:09:59 -0700

    initial commit

---
+++ StackOverflow/Models/Answer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackOverflow.Models
+{
+    [Table("Answers")]
+    public class Answer
+    {
+        [Key]
+        public int AnswerId { get; set; }
+        public int QuestionId { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public int VoteTally { get; set; } = 0;
+        public bool Best { get; set; } = false;
+    }
+}
---
+++ StackOverflow/Models/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackOverflow.Models
+{
+    [Table("Questions")]
+    public class Question
+    {
+        [Key]
+        public int QuestionId { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int VoteTally { get; set; } = 0;
+
+        public virtual List<Answer> Answers { get; set; }
+    }
+}
