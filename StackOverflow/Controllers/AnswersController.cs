c3224c794e521422ea8fdc00a0f5c7830447686f
Sara Hamilton and Stephanie Faber <sara@indeterminate.net and eyesicedover@gmail.com>
2018-04-30 14:26:00 -0700

    add working question detail view

--- StackOverflow/Controllers/AnswersController.cs
+++ StackOverflow/Controllers/AnswersController.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using StackOverflow.Models;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
+using System.Linq;
 
 namespace StackOverflow.Controllers
 {
     [Authorize]
-    public class QuestionsController : Controller
+    public class AnswersController : Controller
     {
         private readonly ApplicationDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public QuestionsController (UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        public AnswersController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
         {
             _userManager = userManager;
             _db = db;
@@ -26,7 +24,7 @@ namespace StackOverflow.Controllers
         {
             var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             var currentUser = await _userManager.FindByIdAsync(userId);
-            return View(_db.Questions.Where(x => x.User.Id == currentUser.Id));
+            return View(_db.Answers.Where(x => x.User.Id == currentUser.Id));
         }
 
         public IActionResult Create()
@@ -35,23 +33,14 @@ namespace StackOverflow.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Question question)
+        public async Task<IActionResult> Create(Answer answer)
         {
             var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             var currentUser = await _userManager.FindByIdAsync(userId);
-            question.User = currentUser;
-            _db.Questions.Add(question);
+            answer.User = currentUser;
+            _db.Answers.Add(answer);
             _db.SaveChanges();
             return RedirectToAction("Index");
-        }
-
-        public async Task<IActionResult> Details(int id)
-        {
-            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var currentUser = await _userManager.FindByIdAsync(userId);
-            return View(_db.Questions.Where(x => x.QuestionId == id));
-            //var model = _db.Questions.Where(x => x.QuestionId == id);
-            //return View(model);
         }
     }
 }
