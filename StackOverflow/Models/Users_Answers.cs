627a62dd4b424970d4439d7008e27fb546527bb2
Sara Hamilton and Stephanie Faber <sara@indeterminate.net and eyesicedover@gmail.com>
2018-05-01 16:19:28 -0700

    add Users_Answers model

---
+++ StackOverflow/Models/Users_Answers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackOverflow.Models
+{
+    [Table("Users_Answers")]
+    public class Users_Answers
+    {
+        [Key]
+        public int UAKey { get; set; }
+        [ForeignKey("UserId")]
+        public string UserId { get; set; }
+        [ForeignKey("AnswerId")]
+        public string AnswerId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+        public virtual List<Answer> Answers { get; set; }
+    }
+}
